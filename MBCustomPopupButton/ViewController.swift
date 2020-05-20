b70940c9d4b4fe66f29fc909abad32d880dd86b3
viorel <viorel@minglebit.com>
2020-05-20 23:42:46 +0300

    Initial Commit

---
+++ MBCustomPopupButton/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MBCustomPopupButton
+//
+//  Created by Viorel Porumbescu on 20/05/2020.
+//  Copyright © 2020 Minglebit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
